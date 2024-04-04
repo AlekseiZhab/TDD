2fd5fd3f2fba0aa82ad0860c0d24b01b54bf1d44
Aleksei <aleksei.zh@mail.ru>
2024-04-04 12:43:53 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,8 @@
 package org.example;
 
+
 public class PhoneBook {
+    public String add(String name, String number) {
+        return "0";
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,15 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Assertions;
+
+
 public class PhoneBookTest {
+    @org.junit.jupiter.api.Test
+    public void testPhonebookAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Алексей", "1234567890");
+        String username = phoneBook.add("Алексей", "1234567890");
+        Assertions.assertEquals(phoneBook, username);
+
+
+    }
 }
