9201d70d92ca5548c928455cb604c8a19871632e
Aleksei <aleksei.zh@mail.ru>
2024-04-04 13:09:05 +0300

    Stub and tests for findByNumber

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -7,4 +7,7 @@ public class PhoneBook {
     public void add(String name, String number) {
         contacts.put(name, number);
     }
+    public String findByNumber(String number) {
+        return null;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -8,9 +8,16 @@ public class PhoneBookTest {
     public void testPhonebookAdd() {
         PhoneBook phoneBook = new PhoneBook();
         phoneBook.add("Алексей", "1234567890");
-        String username = phoneBook.contacts.get("Алексей");
-        Assertions.assertEquals("1234567890", username);
+        String userName = phoneBook.contacts.get("Алексей");
+        Assertions.assertEquals("1234567890", userName);
 
 
+    }
+    @org.junit.jupiter.api.Test
+    public void testPhonebookFindByNumber() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Алексей", "1234567890");
+        String number = phoneBook.findByNumber("1234567890");
+        Assertions.assertEquals("Алексей", number);
     }
 }
